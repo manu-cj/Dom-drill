830f6560276d288dcfd84681cd49768cee5833dd
manu-cj <carenjotje@gmail.com>
2024-04-28 13:22:28 +0200

    not extra done

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 let ul = document.querySelector('ul');
 let li = document.querySelectorAll('li');
 
-console.log(ul.childNodes);
+let double = []
 
 for (let i = 0; i < ul.childNodes.length; i++) {
-    element = ul.childNodes[i]
-   
-    if (element.nodeType === 1) {
-        if (element.textContent.includes('Fast and Furious')) {
-            console.log('hey');
-            if (i !== 0) {
-                ul.insertBefore(element, ul.firstChild);
-            }
-            break;
-        }
-        
-    }
+    let element = ul.childNodes[i];
 
+    if (element.nodeType === 1) {
+        element.addEventListener('click', function() {
+            if (element.textContent === 'Fast and Furious') {
+                alert("The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.");
+            } else {
+                alert(element.textContent);
+            }
+        });
+        if (double.includes(element.textContent)) {
+            ul.removeChild(element);
+        } else {
+            double.push(element.textContent);
+            if (element.textContent.includes('Fast and Furious')) {
+                if (i !== 0) {
+                    ul.insertBefore(element, ul.firstChild);
+                    element.classList.add('important');
+                }
+            }
+        }
+    }
 }
